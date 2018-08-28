5bffc74e79f3288ae4959fef8de0f721d1fb70e2
sleeplesseditor <josh@bryson.me.uk>
2018-08-29 00:04:31 +0100

    Mongoose schema setup

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 
 require('dotenv').config({ path: './variables.env' });
 
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+//Connect to MLab database
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
@@ -10,6 +14,7 @@ mongoose
     })
     .catch(err => console.error(err));
 
+//Initialize App Server
 const app = express();
 const PORT = process.env.PORT || 4444;
 
