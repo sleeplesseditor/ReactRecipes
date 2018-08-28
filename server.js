670d0cc38d88fa99c55ac2a1f0f21c7c78c3afa1
sleeplesseditor <josh@bryson.me.uk>
2018-08-28 23:56:57 +0100

    MLab connection setup

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config({ path: './variables.env' });
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('DB Connected')
+    })
+    .catch(err => console.error(err));
 
 const app = express();
 const PORT = process.env.PORT || 4444;
