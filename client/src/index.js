575519b16a427783bc2f0475b23bf0349de7a7c7
sleeplesseditor <josh@bryson.me.uk>
2018-08-30 00:07:20 +0100

    React connected to GraphQL and styling updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const client = new ApolloClient({
+    uri: 'http://localhost:4444/graphql'
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require('dotenv').config({ path: './variables.env' });
 
@@ -30,6 +31,13 @@ mongoose
 
 //Initialize App Server
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 
 //Create GraphiQL Application
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
