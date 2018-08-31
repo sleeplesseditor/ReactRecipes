d3b55c01934190dbb2cba4154493749cc7c56c31
sleeplesseditor <josh@bryson.me.uk>
2018-08-31 20:22:59 +0100

    Basic routing setup

---
+++ client/src/components/Auth/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Signup extends React.Component {
+    render() {
+        return (
+            <div>
+                Sign Up
+            </div>
+        )
+    }
+}
+
+export default Signup;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import { 
+    BrowserRouter as Router, 
+    Redirect,
+    Route, 
+    Switch 
+} from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+
+import './index.css';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
 
 const client = new ApolloClient({
     uri: 'http://localhost:4444/graphql'
 });
 
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Redirect to="/" />
+        </Switch>
+    </Router>
+);
+
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <App />
+        <Root />
     </ApolloProvider>, 
     document.getElementById('root')
 );
