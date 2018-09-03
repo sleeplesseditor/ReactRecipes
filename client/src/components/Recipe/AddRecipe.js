1dd020c036f5f2d1d990c8bfccc814ddf8dcdbd5
sleeplesseditor <josh@bryson.me.uk>
2018-09-04 00:01:49 +0100

    Add Recipe component setup

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
 const AddRecipe = () => (
-    <div>Add Recipe</div>
+    <div className="App">
+        <h2 className="App">Add Recipe</h2>
+        <form className="form">
+            <input 
+                type="text" 
+                name="name" 
+                placeholder="Recipe Name" 
+                onChange={this.handleChange} 
+            />
+            <select name="category" onChange={this.handleChange}>
+                <option value="Breakfast">Breakfast</option>
+                <option value="Lunch">Lunch</option>
+                <option value="Dinner">Dinner</option>
+                <option value="Snack">Snack</option>
+            </select>        
+            <input 
+                type="text" 
+                name="description" 
+                placeholder="Add Description"
+                onChange={this.handleChange}
+            />
+            <textarea 
+                type="text" 
+                name="instructions" 
+                placeholder="Add Instruction"
+                onChange={this.handleChange}
+            />
+            <button 
+                type="submit" 
+                className="button-primary"
+            >
+                Submit
+            </button>
+        </form>
+    </div>
 );
 
 export default AddRecipe;
--- client/src/components/Recipe/RecipePage.js
+++ client/src/components/Recipe/RecipePage.js
@@ -10,7 +10,17 @@ const RecipePage = ({ match }) => {
             {({ data, loading, error }) => {
                 if(loading) return <div>Loading...</div>
                 if(error) return <div>Error</div>
-                return <div>Recipe Page</div>
+                return (
+                    <div className="App">
+                        <h2>{data.getRecipe.name}</h2>
+                        <p>Category: {data.getRecipe.category}</p>
+                        <p>Description: {data.getRecipe.description}</p>
+                        <p>Instructions: {data.getRecipe.instructions}</p>
+                        <p>Likes: {data.getRecipe.likes}</p>
+                        <p>Created By: {data.getRecipe.username}</p>
+                        <button>Like</button>
+                    </div>
+                );
             }}
         </Query> 
     );
