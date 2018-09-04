65ffa907c715a54c53bfc6400533e0ac9434b7ab
sleeplesseditor <josh@bryson.me.uk>
2018-09-05 00:44:01 +0100

    Create Recipe query setup

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,41 +1,124 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Mutation } from 'react-apollo';
+import { ADD_RECIPE, GET_ALL_RECIPES } from '../../queries';
+import Error from '../Error';
 
-const AddRecipe = () => (
-    <div className="App">
-        <h2 className="App">Add Recipe</h2>
-        <form className="form">
-            <input 
-                type="text" 
-                name="name" 
-                placeholder="Recipe Name" 
-                onChange={this.handleChange} 
-            />
-            <select name="category" onChange={this.handleChange}>
-                <option value="Breakfast">Breakfast</option>
-                <option value="Lunch">Lunch</option>
-                <option value="Dinner">Dinner</option>
-                <option value="Snack">Snack</option>
-            </select>        
-            <input 
-                type="text" 
-                name="description" 
-                placeholder="Add Description"
-                onChange={this.handleChange}
-            />
-            <textarea 
-                type="text" 
-                name="instructions" 
-                placeholder="Add Instruction"
-                onChange={this.handleChange}
-            />
-            <button 
-                type="submit" 
-                className="button-primary"
+const initialState = {
+    name: "",
+    instructions: "",
+    category: "Breakfast",
+    description: "",
+    username: ""
+};
+
+class AddRecipe extends Component {
+    state = { ...initialState };
+
+    clearState = () => {
+      this.setState({ ...initialState });
+    };
+
+    componentDidMount() {
+        this.setState({
+            username: this.props.session.getCurrentUser.username
+        });
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+        console.log(name, ":", value);
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = (event, addRecipe) => {
+        event.preventDefault();
+        addRecipe().then(({ data }) => {
+            console.log(data);
+            this.clearState();
+            this.props.history.push("/");
+        });
+    };
+
+    validateForm = () => {
+        const { name, category, description, instructions } = this.state;
+        const isInvalid = !name || !category || !description || !instructions;
+        return isInvalid;
+    }
+
+    updateCache = (cache, { data: { addRecipe } }) => {
+        const { getAllRecipes } = cache.readQuery({ query: GET_ALL_RECIPES })
+        
+        cache.writeQuery({
+            query: GET_ALL_RECIPES,
+            data: {
+                getAllRecipes: [addRecipe, ...getAllRecipes]
+            }
+        });
+    };
+
+    render() {
+        const { name, category, description, instructions, username } = this.state;
+
+        return (
+            <Mutation 
+                mutation={ADD_RECIPE} 
+                variables={{ name, category, description, instructions, username }}
+                update={this.updateCache}
             >
-                Submit
-            </button>
-        </form>
-    </div>
-);
+                {( addRecipe, { data, loading, error}) => {
+                    return (
+                        <div className="App">
+                            <h2 className="App">Add Recipe</h2>
+                            <form className="form" onSubmit={event => this.handleSubmit(event, addRecipe)}>
+                                <input 
+                                    type="text" 
+                                    name="name" 
+                                    placeholder="Recipe Name" 
+                                    onChange={this.handleChange}
+                                    value={name} 
+                                />
+                                <select 
+                                    name="category" 
+                                    onChange={this.handleChange}
+                                    value={category} 
+                                >
+                                    <option value="Breakfast">Breakfast</option>
+                                    <option value="Lunch">Lunch</option>
+                                    <option value="Dinner">Dinner</option>
+                                    <option value="Snack">Snack</option>
+                                </select>        
+                                <input 
+                                    type="text" 
+                                    name="description" 
+                                    placeholder="Add Description"
+                                    onChange={this.handleChange}
+                                    value={description} 
+                                />
+                                <textarea 
+                                    type="text" 
+                                    name="instructions" 
+                                    placeholder="Add Instruction"
+                                    onChange={this.handleChange}
+                                    value={instructions} 
+                                />
+                                <button 
+                                    disabled={loading || this.validateForm}
+                                    type="submit" 
+                                    className="button-primary"
+                                >
+                                    Submit
+                                </button>
+                                {error && <Error error={error} />}
+                            </form>
+                        </div>
+                    );
+                }}
+            </Mutation>
+        );
+    }
+};
 
-export default AddRecipe;
+export default withRouter(AddRecipe);
