caacda95ae98b771122174b4157261bc36042fdd
sleeplesseditor <josh@bryson.me.uk>
2018-09-12 23:03:37 +0100

    Pre-Heroku deployment setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
@@ -66,7 +67,15 @@ app.use('/graphql', bodyParser.json(), graphqlExpress(({ currentUser }) => ({
 );
 
 //Create GraphiQL Application
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 4444;
 
